dbcf3e149cb09d745c06b4b5c1e2ea3f66a61744
abdelfaizan <abdelfaizan@gmail.com>
2017-01-21 16:08:53 +0100

    Page creation de don en cours

--- app/templates/page/creation_don.php
+++ app/templates/page/creation_don.php
@@ -3,13 +3,16 @@
 <?php $this->start('main_content') ?>
 
 <section id="portfolio" class="padding">
+
   <div class="container">
+
     <form>
+
       <div class="form-group">
         <label for="dons">Je donne:</label>
-        <textarea type="text" name="dons" class="form-control" id="dons" placeholder="Ex: Une barquette de 4 tomates.">
-        </textarea>
+        <textarea type="text" name="dons" class="form-control" id="dons" placeholder="Ex: Une barquette de 4 tomates."></textarea>
       </div>
+
       <select class="form-control">
         <option>Mots clés:</option>
         <option>Fruits</option>
@@ -17,27 +20,45 @@
         <option>Boissons</option>
         <option>Produits laitiers</option>
       </select>
-      <label class="radio-inline">
-        <input type="radio" name="borne" id="borne" value="borne"> Sélectionner une borne réfrigérée
-      </label>
-      <label class="radio-inline">
-        <input type="radio" name="adressePerso" id="adressePerso" value="adresse"> Mon adresse
-      </label>
-      <h3>Mon adresse :</h3>
-      <p>Adresse dfhfdhvs</p>
+
+      <div class="form-group">
+        <h3>Sélectionner une adresse:</h3>
+      </div>
+
+      <div class="form-group">
+        <label class="radio-inline">
+          <input type="radio" name="borne" id="borne" value="borne"> Sélectionner une borne réfrigérée
+        </label>
+
+        <label class="radio-inline">
+          <input type="radio" name="adressePerso" id="adressePerso" value="adresse"> Mon adresse
+        </label>
+      </div>
+
+
+      <div class="form-group">
+        <h3>Mon adresse :</h3>
+        <p>Adresse dfhfdhvs</p>
+      </div>
+
+
       <div class="form-group">
         <label for="acces">Jours et horaires d'accés:</label>
-        <input type="text" name="acces" class="form-control" id="acces" placeholder="Ex: Lundi, Mardi, Mercredi, etc...">
+        <textarea type="text" name="acces" class="form-control" id="acces" placeholder="Ex: Lundi, Mardi, Mercredi, etc..."></textarea>
       </div>
+
       <div class="form-group">
       <label for="numero">Votre numéro de téléphone:</label>
         <input type="text" name="numero" class="form-control" id="numero" placeholder="Ex: 01 02 03 04 05">
       </div>
+
       <div class="form-group">
         <label for="images">Charger une image du produit:</label>
         <input type="file" id="images">
       </div>
+
       <button type="submit" class="btn btn-default">Submit</button>
+
     </form>
 
   </div>
