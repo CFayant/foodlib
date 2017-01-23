0d865ff7c2e7268dc26a69789c822a0c1ecbdfa7
abdelfaizan <abdelfaizan@gmail.com>
2017-01-23 16:49:49 +0100

    reset

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -7,17 +7,10 @@ use \W\Controller\Controller;
 class DefaultController extends Controller
 {
 
-	/**
-	 * Page d'accueil par défaut
-	 */
+	/*Page d'accueil par défaut */
 	public function home()
 	{
 		$this->show('page/home');
-	}
-
-	public function inscription()
-	{
-		$this->show('page/inscription');
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,7 @@
 <?php
-	
+
 	$w_routes = array(
 
-		['GET', '/home/', 'Default#home', 'home'],
-		['GET', '/inscription/', 'Default#inscription', 'inscription'],
-		['GET', '/profil/', 'Default#profil', 'profil'],
-
-
-		
+		['GET', '/home/', 'Default#home', 'home']
 
 	);
