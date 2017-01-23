19d98cc74c90ef6dd912b7f0c070b7fe66ea7570
abdelfaizan <abdelfaizan@gmail.com>
2017-01-23 14:35:22 +0100

    page creationDon V4

--- app/templates/page/creation_don.php
+++ app/templates/page/creation_don.php
@@ -45,6 +45,7 @@
 
       <div class="form-group form_center text-center">
         <p>Adresse du donneur</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58547.93527301496!2d2.3465382607150715!3d48.856049443109214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis!5e0!3m2!1sfr!2sfr!4v1485169092765" width="600" height="450" frameborder="0" style="border:0" allowfullscreen class="img-responsive"></iframe>
       </div>
 
 
