5226436e5f3446ef0fba271815eacfab262dafde
Etudiant <Etudiant>
2017-01-24 12:25:19 +0100

    ajout page profil_d

---
+++ app/templates/page/profil_d.php
@@ -0,0 +1,43 @@
+<?php $this->layout('layout', ['title' => 'Profil']) ?>
+
+<?php $this->start('main_content') ?>
+
+<section class="container padding text-center">
+
+	<h1>Mon Profil</h1>
+
+	<caption class="">
+		
+		<img src="" alt="img_profil">
+
+	</caption>
+
+</section>
+
+<section class="container text-center">
+
+<h3>Mes Dons</h3><br>
+
+	<table class="table">
+		
+		<tr>
+			
+			<th>Date d'acquisition</th>
+
+			<th>Descriptif du produit</th>
+
+		</tr>
+
+		<tr>
+			
+			<td>Lorem ipsum dolor sit amet.</td>
+			<td>Lorem ipsum dolor sit amet.</td>
+
+		</tr>
+
+	</table>
+	
+
+</section>
+
+<?php $this->stop('main_content') ?>
