d036ff6cfa31f923e30fb7facf776e12cd52b087
Etudiant <Etudiant>
2017-01-25 17:55:11 +0100

    ajout bdd formulaire LD

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace Controller;
 
 use \W\Controller\Controller;
+use \Manager\TypeDateManager;
+use \Manager\BorneManager;
+use \Manager\DonManager;
 use \GUMP;
 
 class DefaultController extends Controller
@@ -65,39 +68,65 @@ public function creationDon()
 	$erreurs = [];
 	$gump = new GUMP();
 
+	$type_date_manager = new TypeDateManager();
+	$type_date_manager->setTable('type_date');
+	$liste_type_date = $type_date_manager->findAll();
+
+	$bornes_manager = new BorneManager();
+	$bornes = $bornes_manager->findAll();
+
+	$dons_manager = new DonManager();
+
 	if(isset($_POST['donner'])) {
-
-		$_POST = $gump->sanitize($_POST);
-
-		$gump->validation_rules(array(
-			'dons'                => 'required|alpha_numeric|max_len,500|min_len,6',
-			'acces'               => 'required|alpha_numeric|max_len,500|min_len,6',
-			'numero'              => 'required|numeric|exact_len,10',
-			'date_consommation'   => 'required|date'
-
-        //'image'   => 'required_file|extension,png;jpg'
-			));
-
-		$gump->filter_rules(array(
-			'dons'    => 'trim|sanitize_string',
-			'acces'   => 'trim|sanitize_string',
-			'numero'  => 'trim|sanitize_numbers'
-			));
-
-		$validated_data = $gump->run($_POST);
-
-		if($validated_data === false)
-
-		{
-			$erreurs = $gump->get_errors_array();
+		$uploads_dir = '/var/www/public/assets/uploads';
+		foreach ($_FILES['myform']["error"] as $key => $error) {
+		    if ($error == UPLOAD_ERR_OK) {
+		        $tmp_name = $_FILES["myform"]["tmp_name"][$key];
+		        $name = $_FILES["myform"]["name"][$key];
+		        move_uploaded_file($tmp_name, "$uploads_dir/$name");
+		    }
 		}
-		else
-		{
-        print_r($validated_data); // validation successful
-    	}
+		$data = array_merge($_POST['myform'], ['donneur_id' => 1, 'image' => $name]); // TODO : $_SESSION['user']['id']
+		// print_r($data);
+		// die();
+
+		// $_POST = $gump->sanitize($_POST);
+
+		// $gump->validation_rules(array(
+		// 	'dons'                => 'required|alpha_numeric|max_len,500|min_len,6',
+		// 	'acces'               => 'required|alpha_numeric|max_len,500|min_len,6',
+		// 	'numero'              => 'required|numeric|exact_len,10',
+		// 	'date_consommation'   => 'required|date'
+
+  //       //'image'   => 'required_file|extension,png;jpg'
+		// 	));
+
+		// $gump->filter_rules(array(
+		// 	'dons'    => 'trim|sanitize_string',
+		// 	'acces'   => 'trim|sanitize_string',
+		// 	'numero'  => 'trim|sanitize_numbers'
+		// 	));
+
+		// $validated_data = $gump->run($_POST);
+
+		// if($validated_data === false)
+
+		// {
+		// 	$erreurs = $gump->get_errors_array();
+		// }
+		// else
+		// {
+  //       print_r($validated_data); // validation successful
+  //   	}
+
+
+		$dons_manager->insert($data);
+
+		$this->redirectToRoute('profil_d');
+
 	}
 
-	$this->show('page/creation_don', ['erreurs' => $erreurs]);
+	$this->show('page/creation_don', ['erreurs' => $erreurs, 'liste_type_date' => $liste_type_date, 'bornes' => $bornes]);
 
 }
 
--- app/templates/page/creation_don.php
+++ app/templates/page/creation_don.php
@@ -7,30 +7,25 @@
   <div class="container">
     <h2 class="text-center">Je fais un don:</h2>
 
-      <?php if(!empty($erreurs)) { ?>
-
-      <?php print_r($erreurs) ?>
-          erreurs...
-      <?php } ?>
-
     <form method="POST" class="col-lg-6 margin-auto" enctype="multipart/form-data">
 
       <div class="form-group form_center">
-        <label for="dons">Je donne:</label>
-        <?php if(isset($erreurs['Dons'])) : ?>
-          <span class="text-danger"><?= $erreurs['Dons'] ?></span>
+        <label for="titre">Je donne:</label>
+        <?php if(isset($erreurs['Titre'])) : ?>
+          <span class="text-danger"><?= $erreurs['Titre'] ?></span>
         <?php endif ?>
-        <textarea type="text" name="myform[dons]" class="form-control" id="dons" placeholder="Ex: Une barquette de 4 tomates."></textarea>
+        <textarea type="text" name="myform[titre]" class="form-control" id="titre" placeholder="Ex: Une barquette de 4 tomates."></textarea>
       </div>
 
       <div class="form-group form_center">
         <label for="tags">La catégories:</label>
-        <select class="form-control" name="myform[tags]">
+        <select class="form-control" name="tags">
+          <!-- TODO : foreach avec table tags -->
           <option value="">Mots clés:</option>
-          <option value="fruits">Fruits</option>
-          <option value="légumes">Légumes</option>
-          <option value="boissons">Boissons</option>
-          <option value="laitiers">Produits laitiers</option>
+          <option value="1">Fruits</option>
+          <option value="2">Légumes</option>
+          <option value="3">Boissons</option>
+          <option value="4">Produits laitiers</option>
         </select>
       </div>
 
@@ -40,7 +35,7 @@
 
       <div class="form-group form_center text-center">
         <label class="radio-inline">
-          <input type="radio" name="myform[adress]" value="borne"> Sélectionner une borne réfrigérée
+          <input type="radio" name="myform[adress]" value="borne" checked> Sélectionner une borne réfrigérée
         </label>
 
         <label class="radio-inline">
@@ -53,17 +48,18 @@
       </div>
 
       <div class="form-group form_center text-center">
-        <select class="form-control">
+        <select class="form-control" name="myform[borne_id]">
           <option value="">Les bornes:</option>
-          <option value="borne1">Borne 1</option>
-          <option value="borne2">Borne 2</option>
-          <option value="borne3">Borne 3</option>
-          <option value="borne4">Borne 4</option>
+          <?php foreach ($bornes as $borne): ?>
+            <option value="<?= $borne['id'] ?>"><?= $borne['adresse_borne'] . " " . $borne['cp_borne'] ?></option>
+          <?php endforeach ?>
+          
+          
         </select>
         <p>Adresse du donneur</p>
       </div>
 
-      <div class="form-group form_center">
+     <!--  <div class="form-group form_center">
         <label for="acces">Jours et horaires d'accés:</label>
         <?php if(isset($erreurs['Acces'])) : ?>
           <span class="text-danger"><?= $erreurs['Acces'] ?></span>
@@ -77,19 +73,21 @@
           <span class="text-danger"><?= $erreurs['Numero'] ?></span>
         <?php endif ?>
         <input type="text" name="myform[numero]" class="form-control" id="numero" placeholder="Ex: 01 02 03 04 05">
-      </div>
+      </div> -->
 
       <div class="form-group form_center">
         <label for="image">Charger une image du produit:</label>
-        <input type="file" id="images" name="myform[image]">
+        <input type="file" id="image" name="myform[image]">
       </div>
 
       <div class="form-group form_center">
         <label for="type_date">Le type de date de consommation:</label>
-        <select class="form-control">
+        <select class="form-control" name="myform[type_id]">
           <option>Type de date:</option>
-          <option value="durabilité minimale">Date de durabilité minimale</option>
-          <option value="limite de consommation">Date limite de consommation</option>
+          <!-- todo foreach sur la table type_date -->
+          <?php foreach($liste_type_date as $type): ?>
+          <option value="<?= $type['id'] ?>"><?= $type['libelle_date']?></option>
+         <? endforeach ?>
         </select>
       </div>
 
@@ -102,7 +100,7 @@
       </div>
 
       <div class="col-md-12 text-center">
-        <button type="submit" name="myform[donner]" class="btn btn-default donner" >Je donne</button>
+        <button type="submit" name="donner" class="btn btn-default donner" >Je donne</button>
 
         <!-- data-toggle="modal" data-target="#donner" -->
       </div>
