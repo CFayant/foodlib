6fb2e38184ef1fde383303c866457a640336a599
Etudiant <Etudiant>
2017-01-30 16:51:55 +0100

    c'est lourd putain

--- app/Controller/ConnexionController.php
+++ app/Controller/ConnexionController.php
@@ -8,32 +8,26 @@ use \W\Manager\UserManager;
 
 class ConnexionController extends Controller {
 
-	// public function displayLogin() {
-
-		
-	// }
-
 	public function login() {
 
+		if(isset($_POST['connexion'])) {
 
+			$auth = new AuthentificationManager();
+			$userManager = new UserManager();
 
+			if($auth->isValidLoginInfo($_POST['myform']['username'], $_POST['myform']['password'])) {
 
-		// if(isset($_POST['connexion'])) {
+				$user = $userManager->getUserByUsernameOrEmail($_POST['myform']['username']);
+				print_r($user);
 
-		// 	$auth = new AuthentificationManager();
-		// 	$userManager = new UserManager();
+				$auth->logUserIn($user);
+				$this->redirectToRoute('home');
+			}
 
-		// 	if($auth->isValidLoginInfo($_POST['myform']['username'], $_POST['myform']['password'])) {
+		}
 
-		// 		$user = $userManager->getUserByUsernameOrEmail($_POST['myform']['username']);
-		// 		$auth->logUserIn($user);
-
-		// 		$this->redirectToRoute('home');
-		// 	}
-
-
-		// }
 	$this->show('page/login');
+
 	}
 
 	public function logout() {
